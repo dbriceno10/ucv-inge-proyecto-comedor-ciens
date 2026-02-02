601ff660f21fa80bf527fa1e6c2107d069b159b9
Daniel Briceno <daniel.b100726@gmail.com>
2026-02-01 22:23:31 -0400

    User Model:

---
+++ src/Model/User/UserModel.java
@@ -0,0 +1,112 @@
+package Model.User;
+
+public class UserModel {
+  private Number id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+  private String role; // 'ADMIN' | 'COMMENSAL'
+  private Boolean status;
+  private String createdAt;
+  private String updatedAt;
+  private String deletedAt = null;
+
+  public UserModel() {
+  }
+
+  public UserModel(Number id, String firstName, String lastName, String email, String password, String role,
+      Boolean status, String createdAt, String updatedAt, String deletedAt) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+    this.status = status;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
+
+  // Getters y setters
+  public Number getId() {
+    return id;
+  }
+
+  public void setId(Number id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public Boolean getStatus() {
+    return status;
+  }
+
+  public void setStatus(Boolean status) {
+    this.status = status;
+  }
+
+  public String getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(String createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public String getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(String updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public String getDeletedAt() {
+    return deletedAt;
+  }
+
+  public void setDeletedAt(String deletedAt) {
+    this.deletedAt = deletedAt;
+  }
+}
