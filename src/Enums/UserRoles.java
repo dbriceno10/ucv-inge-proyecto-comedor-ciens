0435b1cae90d86b140e1f34bf8b0b3206ed98015
Daniel Briceño <77646379+dbriceno10@users.noreply.github.com>
2026-02-04 17:15:33 -0400

    Update UserRoles.java
    Revert changes in enum roles.

--- src/Enums/UserRoles.java
+++ src/Enums/UserRoles.java
@@ -1,6 +1,6 @@
 package Enums;
 
 public class UserRoles {
-  public static final String ADMIN = "ADMINISTRADOR";
-  public static final String COMMENSAL = "COMENSAL";
+  public static final String ADMIN = "ADMIN";
+  public static final String COMMENSAL = "COMMENSAL";
 }
