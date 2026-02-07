34696422adecaa05ac12e11943097ef4eadc6825
seeu <josealejandro2006dosreis@gmail.com>
2026-02-07 18:47:56 -0400

    Improvements to the Dashboard view.

--- src/Controllers/AuthControllers/LoginController.java
+++ src/Controllers/AuthControllers/LoginController.java
@@ -14,7 +14,7 @@ import java.awt.event.ActionEvent;
 public class LoginController implements ActionListener {
     private LoginView view;
 
-    LoginController(LoginView view) {
+    public LoginController(LoginView view) {
         this.view = view;
 
         this.view.loginListener(this);
